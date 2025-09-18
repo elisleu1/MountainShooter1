879b9e6ccfac0f55de102dce7a4facd8756c55da
Eliseu <elisleu1@gmail.com>
2025-09-18 14:11:36 -0300

    third commit creating main loop connecting menu classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
         self.window = None
 
     def run(self, ):
-        pass
+        pygame.init()  # Starts pygame
+        window = pygame.display.set_mode(size=(600, 480))  # Create window run
+
+        while True:  # Loop that's maintain window run
+            menu = Menu(self.window)    # Connecting class menu
+            menu.run()
+            # Check for all events
+            # for event in pygame.event.get():
+            #     if event.type == pygame.QUIT:
+            #         pygame.quit()  # Close window
+            #         quit()  # End pygame
+            # pass
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import pygame
+from code.game import Game
 
-pygame.init()  # Starts pygame
-window = pygame.display.set_mode(size=(600, 480))  # Create window run
-
-while True: # Loop that's maintain window run
-# Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()   # Close window
-            quit()  # End pygame
+game = Game()
+game.run()
